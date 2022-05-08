35cdc956fddf3f7ce828c9419c51369cc171a86c
Meghnameghu <105042430+Meghnameghu@users.noreply.github.com>
2022-05-08 12:52:53 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,6 +1,32 @@
-#include "stats.h"
+#include "stats.h"#*
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<___>& ) {#*
     //Implement statistics here
-    int x=0;
+    int x=0;#*
+}#*
+#include <iostream>
+using namespace std;
+
+int main(){
+    intI=1;
+    int n;
+    
+    while(cin..n){
+        int min;
+        int max;
+        cin>>max;
+        
+        for(intj=0; j<n-1;J++){
+            int temp;
+            cin>>temp;
+            if(temp<min){
+                min=temp;
+            }
+            if(temp>max){
+                max=temp;
+            }
+        }
+        cout<<"case"<<i<<":"<<min<<""<<max<<""<<max-min<<endl:
+        i++;
+    }
 }
