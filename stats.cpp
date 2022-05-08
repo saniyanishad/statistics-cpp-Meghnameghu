0b6b5f532e468fbe47d5467c19170ea546d5d36e
Meghnameghu <105042430+Meghnameghu@users.noreply.github.com>
2022-05-08 20:26:46 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -4,8 +4,10 @@
 #include<bits/stdc++.h>
 //Stats Statistics::Computedstatistics(const std::vector<___>&
 template<typename T> Statistics::Stats<T> Statistics::ComputeStatistics(const std::vector<T>& data)
+template<typename alerters> Statistics::Stats<alerters> Statistics::ComputeStatistics(const std::vector<IAlerter>& emailAlert,&ledAlert)
    {
     Stats<T> st;
+    
     if(data.size()==0)
     {
        st.Average=NAN;
