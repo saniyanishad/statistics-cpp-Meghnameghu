240d9bf9a1c9e6b49d0cba61069a077e788d5f04
Meghnameghu <105042430+Meghnameghu@users.noreply.github.com>
2022-05-08 16:36:19 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -4,20 +4,19 @@
 #include<bits/stdc++.h>
 
 template<typename T> Statistics::Stats<>::Stats(){}
-template<typename T> Statistics::Stats<T,T,T>::Stats(T average,T max,T min):average(average),max(max),min(Min){}
+template<typename T> Statistics::Stats<T,T,T>::Stats(T Average,T Max,T Min):Average(Average),Max(Max),Min(Min){}
 template<typename T> Statistics::Stats<T> Statistics::ComputeStatistics(const std::vector<T>& data)
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
    {
     Stats<T> st;
     if(data.size()==0)
     {
-       st.average=NAN;
-       st.max=NAN;
-       st.min=NAN;
+       st.Average=NAN;
+       st.Max=NAN;
+       st.Min=NAN;
     }
     
-    st.max=*max_element(data.begin(),data.end());
-    st.min=*min_element(data.begin(),data.end());
+    st.Max=*max_element(data.begin(),data.end());
+    st.Min=*min_element(data.begin(),data.end());
     T sum=accumulate(data.begin(), data.end() , 0);
     st.Average=(sum/data.size());
     return st;
